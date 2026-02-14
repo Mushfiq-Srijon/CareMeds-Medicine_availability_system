4af33ef9d0b31bfda6613cd2b0dc51e9eca82adb
SabikunEthika <alam.sabikun2001@gmail.com>
2026-02-14 14:30:25 +0600

    Backend Work

--- server/routes/api.php
+++ server/routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\MedicineController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,5 +22,14 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/session', [SessionController::class, 'getSession']);
+Route::post('/session', [SessionController::class, 'createSession'])->middleware('check.admin');
+Route::put('/session', [SessionController::class, 'updateSession'])->middleware('check.admin');
+Route::post('/sessions', [SessionController::class, 'viewSessions'])->middleware('check.admin');
+Route::post('/attendance', [SessionController::class, 'submitAttendance']);
+Route::get('/medicines', [MedicineController::class, 'index']);
+Route::post('/medicines', [MedicineController::class, 'store']);
