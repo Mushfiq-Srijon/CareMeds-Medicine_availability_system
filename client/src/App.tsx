db5f444c5bbe5ef841e4e943bd318c6f60de10b5
SabikunEthika <alam.sabikun2001@gmail.com>
2026-02-14 22:07:53 +0600

    HomePage and Search Filters

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,9 @@
-import { Outlet, Route, Routes } from 'react-router';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import BaseLayout from './views/BaseLayout';
 import Home from './views/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { Toaster } from 'react-hot-toast';
-import Sessions from './views/Sessions';
 
 function App() {
   return (
@@ -17,16 +16,14 @@ function App() {
             </BaseLayout>
           }
         >
-          <Route path={'/'} element={<Home />} />
-          <Route path={'/sessions'} element={<Sessions />} />
+          <Route path="/" element={<Home />} />
         </Route>
       </Routes>
+
       <Toaster
         position="top-center"
         toastOptions={{
-          error: {
-            duration: 5000,
-          },
+          error: { duration: 5000 },
         }}
       />
     </>
